dafb721092b76ddd2707e2622e8bae2c2e2b5ed2
Nick Lewis <nick@TechEd-0006.techeducators.co.uk>
2023-06-13 16:42:54 +0100

    Update About Me EOD

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # About-Me
+
+An About me Site demonstrating HTML / CSS / JavaScript
+
+!/_SourceFiles/Screenshot 2023-06-13 at 16.39.14.png
--- app.js
+++ app.js
@@ -14,120 +14,78 @@ function myFunc() {
 
     alert("please answer the following with YES/NO or Y/N");
 
-    let myName = prompt("Is my full name - Nicholas John Langford Lewis?");
-        myName = myName.toLowerCase();
-    if (myName === 'yes' || 'y'){
-        console.log("Yes it is! Regal ain't it?!")
-    }else if (myName === 'no' || 'n'){
-        console.log("[X] EEH AWWWWW\nyou are wrong, try again!")
-    }else {
-        console.log("you gotta say - yes / no / y / n - ya dingus!")
+    let myName = prompt("Is my full name - Nicholas John Langford Lewis?").toLowerCase();
+
+while (myName != "yes" && myName != "no" && myName != "y" && myName != "n") {
+  alert("you gotta say - yes/no or y/n only - ya dingus!");
+  myName = prompt("Is my full name - Nicholas John Langford Lewis?").toLowerCase();
 }
 
-let mysteryMan = prompt("Am I a Man of Mystery?");
-mysteryMan = mysteryMan.toLowerCase();
-    if (mysteryMan === 'yes' || 'y'){
-        console.log("Maybe I am, maybe I'm not. Maybe YOU are. Who knows?")
-    }else if (mysteryMan === 'no' || 'n'){
-        console.log("[X] EEH AWWWWW\nyou are wrong, try again!")
-    }else {
-        console.log("you gotta say - yes / no / y / n - ya dingus!")
-    }
-
-
-let norwichFan = prompt("Do I support Norwich City Football Club?");
-norwichFan = norwichFan.toLowerCase();
-    if (norwichFan === 'yes' || 'y'){
-        console.log("[X] EEH AWWWWW\nyou are wrong, try again!")
-    }else if (norwichFan === 'no' || 'n'){
-        console.log("*sings the match of the day theme tune*")
-    }else {
-        console.log("you gotta say - yes / no / y / n - ya dingus!")
-    }
-
-
-let goToUni = prompt("Did I got to University in London?");
-goToUni = goToUni.toLowerCase();
-    if (goToUni === 'yes' || 'y'){
-        console.log("Cockney Rhyming slang for CODE is A LA MODE")
-    }else if (goToUni === 'no' || 'n'){
-        console.log("[X] EEH AWWWWW\nyou are wrong, try again!")
-    }else {
-        console.log("you gotta say - yes / no / y / n - ya dingus!")
-    }
-
-let myGoals = prompt("Do I have any Goals?");
-myGoals = myGoals.toLowerCase();
-    if (myGoals === 'yes' || 'y'){
-        console.log("[X] EEH AWWWWW\nyou are wrong, try again!")
-    }else if (myGoals === 'no' || 'n'){
-        console.log("I haven't got that far yet, sheesh!")
-    }else {
-        console.log("you gotta say - yes / no / y / n - ya dingus!")
-    }
+if (myName === "yes" || myName === "y") {
+  // console.log("Yes it is! Regal ain't it?!");
+  alert("Yes it is! Regal ain't it?!");
+} else if (myName === "no" || myName === "n") {
+  // console.log("[X] EEH AWWWWW\nyou are wrong, try again!");
+  alert("[X] EEH AWWWWW\nyou are wrong!!");
 }
-// let myName = prompt("Is my full name - Nicholas Rupert Langford Lewis?");
 
-// switch (myName.toLowerCase()) {
-//     case "yes" || 'y':
-//         console.log("[X] EEH AWWWWW\nyou are wrong, try again!")
-//         break;
-//     case "no" || 'n':
-//         console.log("Yes it is! Regal ain't it?!")
-//         break;
-//         default:
-//         console.log("Does... Not... Compute :-S. Please try again")
-// }
+let mysteryMan = prompt("Am I a Man of Mystery?").toLowerCase();
 
-// let mysteryMan = prompt("Am I a Man of Mystery?");
+while (mysteryMan != "yes" && mysteryMan != "no" && mysteryMan != "y" && mysteryMan != "n") {
+  alert("you gotta say - yes/no or y/n only - ya dingus!");
+  mysteryMan = prompt("Am I a Man of Mystery?").toLowerCase();
+}
 
-// switch (mysteryMan.toLowerCase()) {
-//     case "yes" || 'y':
-//         console.log("Maybe I am, maybe I'm not. Maybe YOU are. Who knows?")
-//         break;
-//     case "no" || 'n':
-//         console.log("[X] EEH AWWWWW\nyou are wrong, try again!")
-//         break;
-//         default:
-//         console.log("Does... Not... Compute :-S. Please try again")
-// }
+if (mysteryMan === "yes" || mysteryMan === "y") {
+  // console.log("Maybe I am, maybe I'm not. Maybe YOU are. Who knows?");
+  alert("Maybe I am, maybe I'm not. Maybe YOU are. Who knows?");
+} else if (mysteryMan === "no" || mysteryMan === "n") {
+  // console.log("[X] EEH AWWWWW\nyou are wrong, try again!");
+  alert("[X] EEH AWWWWW\nyou are wrong!!");
+}
 
-// let norwichFan = prompt("Do I support Norwich City Football Club?");
+let norwichFan = prompt("Do I support Norwich City Football Club?").toLowerCase();
 
-// switch (norwichFan.toLowerCase()) {
-//     case "yes" || 'y':
-//         console.log("[X] EEH AWWWWW\nyou are wrong, try again!")
-//         break;
-//     case "no" || 'n':
-//         console.log("*sings the match of the day theme tune*")
-//         break;
-//         default:
-//         console.log("Does... Not... Compute :-S. Please try again")
-// }
+while (norwichFan != "yes" && norwichFan != "no" && norwichFan != "y" && norwichFan != "n") {
+  alert("you gotta say - yes/no or y/n only - ya dingus!");
+  norwichFan = prompt("Do I support Norwich City Football Club?").toLowerCase();
+}
 
-// let goToUni = prompt("Did I got to University in London?");
+if (norwichFan === "yes" || norwichFan === "y") {
+  // console.log("[X] EEH AWWWWW\nyou are wrong, try again!");
+  alert("[X] EEH AWWWWW\nyou are wrong!");
+} else if (norwichFan === "no" || norwichFan === "n") {
+  // console.log("*sings the match of the day theme tune*");
+  alert("*sings the match of the day theme tune*");
+}
+   
+let goToUni = prompt("Did I got to University in London?").toLowerCase();
 
-// switch (goToUni.toLowerCase()) {
-//     case "yes" || 'y':
-//         console.log("Cockney Rhyming slang for CODE is A LA MODE")
-//         break;
-//     case "no" || 'n':
-//         console.log("[X] EEH AWWWWW\nyou are wrong, try again!")
-//         break;
-//         default:
-//         console.log("Does... Not... Compute :-S. Please try again")
-// }
+while (goToUni != "yes" && goToUni != "no" && goToUni != "y" && goToUni != "n") {
+  alert("you gotta say - yes/no or y/n only - ya dingus!");
+  goToUni = prompt("Did I got to University in London?").toLowerCase();
+}
 
-// let myGoals = prompt("Do I have any Goals?");
+if (goToUni === "yes" || goToUni === "y") {
+  // console.log("Cockney Rhyming slang for CODE is A LA MODE");
+  alert("Cockney Rhyming slang for CODE is A LA MODE");
+} else if (goToUni === "no" || goToUni === "n") {
+  // console.log("[X] EEH AWWWWW\nyou are wrong, try again!");
+  alert("[X] EEH AWWWWW\nyou are wrong!");
+}
 
-// switch (myGoals.toLowerCase()) {
-//     case "yes" || 'y':
-//         console.log("[X] EEH AWWWWW\nyou are wrong, try again!")
-//         break;
-//     case "no" || 'n':
-//         console.log("I haven't got that far yet, sheesh!")
-//         break;
-//         default:
-//         console.log("Does... Not... Compute :-S. Please try again")
-// }
-// }
+let myGoals = prompt("Do I have any Goals?").toLowerCase();
+
+while (myGoals != "yes" && myGoals != "no" && myGoals != "y" && myGoals != "n") {
+  alert("you gotta say - yes/no or y/n only - ya dingus!");
+  myGoals = prompt("Do I have any Goals?").toLowerCase();
+}
+
+if (myGoals === "yes" || myGoals === "y") {
+  // console.log("[X] EEH AWWWWW\nyou are wrong, try again!");
+  alert("[X] EEH AWWWWW\nyou are wrong!\nThanks for taking the time to get to know me " + user + "!");
+} else if (myGoals === "no" || myGoals === "n") {
+  // console.log("I haven't got that far yet, sheesh!");
+  alert("I haven't got that far yet, sheesh!\nThanks for taking the time to get to know me " + user + "!");
+}
+}
